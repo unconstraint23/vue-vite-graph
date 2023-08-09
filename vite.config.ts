296c7bf67679fd3e0df6cbeb4b91cc9fd12f7c0b
unconstraint23 <527667654@qq.com>
2023-08-09 22:11:33 +0800

    glsl配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import glsl from 'vite-plugin-glsl';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    glsl()
+  ],
   optimizeDeps: {
     include: ['vue-router']
 },
