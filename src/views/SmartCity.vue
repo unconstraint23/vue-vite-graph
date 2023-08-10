34769028cfb524b993f1b6222d341a7df91231e7
unconstraint23 <527667654@qq.com>
2023-08-10 21:51:23 +0800

    模型上色

--- src/views/SmartCity.vue
+++ src/views/SmartCity.vue
@@ -12,14 +12,15 @@ import { onMounted, ref } from 'vue';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls';
+import {meshLine as getMeshLine, modifyCityMaterial } from './utils/meshLine'
     const containerRef = ref<any>(null)
         const axesHelper = new THREE.AxesHelper(5);
     onMounted(() => {
         const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 5000 );
     camera.position.set(5, 10, 15)
-    const light = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
-scene.add(light);
+    const light = new THREE.AmbientLight(0xffffff, 1); // soft white light
+    scene.add(light);
     scene.add(axesHelper)
     // 更新摄像头宽高比例
 camera.aspect = window.innerWidth / window.innerHeight;
@@ -38,7 +39,22 @@ renderer.outputEncoding = THREE.sRGBEncoding
   const createMesh = () => {
     gltfLoader.load("/src/cityModel/city.glb", (gltf) => {
    
-   
+        gltf.scene.traverse((item) => {
+            console.log(item)
+            if(item.type === "Mesh") {
+                const cityMaterial = new THREE.MeshBasicMaterial({
+          color: new THREE.Color(0x0c0e33),
+            });
+            item.material = cityMaterial;
+            modifyCityMaterial(item)
+            if (item.name == "Layerbuildings") {
+            const meshLine = getMeshLine(item.geometry);
+            const size = item.scale.x * 1.001;
+            meshLine.scale.set(size, size, size);
+            scene.add(meshLine);
+        }
+            }
+        })
     scene.add(gltf.scene);
   })
   }
