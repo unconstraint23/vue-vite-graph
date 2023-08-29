751c3e006372e03c31e7b017308526f0ed916a1d
zrj <527667654@qq.com>
2023-08-29 22:55:55 +0800

    feat: 园区样式

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,21 @@
   
 </template>
 
-<style scoped>
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+}
+ul,li{
+  list-style: none;
+}
+
+ html{
+  font-size:calc(100vw / 19.2);
+}
+
+body{
+  font-size: 16px;
+}
 
 </style>
